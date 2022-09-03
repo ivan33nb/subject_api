99ed54c2b9633fd155b457dedea0e87ab8cd0ef5
out-stroki-waisman <k5LpLsM5Kgjy>
2022-09-03 23:18:16 +0300

    Created subject repo

---
+++ src/main/java/com/gmail/ivanvaysman/subject_api/repository/SubjectRepository.java
@@ -0,0 +1,38 @@
+package com.gmail.ivanvaysman.subject_api.repository;
+
+import com.tej.JooQDemo.jooq.sample.model.Tables;
+import com.tej.JooQDemo.jooq.sample.model.tables.pojos.Subject;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class SubjectRepository {
+
+  private final DSLContext dslContext;
+
+  public int insertSubject(Subject subject) {
+    return dslContext
+        .insertInto(
+            Tables.SUBJECT,
+            Tables.SUBJECT.ID,
+            Tables.SUBJECT.NAME,
+            Tables.SUBJECT.COMMENT,
+            Tables.SUBJECT.AMOUNT,
+            Tables.SUBJECT.CREATE_DATE)
+        .values(
+            subject.getId(),
+            subject.getName(),
+            subject.getComment(),
+            subject.getAmount(),
+            LocalDateTime.now())
+        .execute();
+  }
+
+  public List<Subject> getAllSubjects() {
+    return dslContext.selectFrom(Tables.SUBJECT).fetchInto(Subject.class);
+  }
+}
