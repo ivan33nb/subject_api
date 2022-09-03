18f3ff23f9317ed09a2fe7479ae756202d0c96e4
out-stroki-waisman <k5LpLsM5Kgjy>
2022-09-04 00:37:30 +0300

    Added subject update method in repo

--- src/main/java/com/gmail/ivanvaysman/subject_api/repository/SubjectRepository.java
+++ src/main/java/com/gmail/ivanvaysman/subject_api/repository/SubjectRepository.java
@@ -2,7 +2,6 @@ package com.gmail.ivanvaysman.subject_api.repository;
 
 import com.tej.JooQDemo.jooq.sample.model.Tables;
 import com.tej.JooQDemo.jooq.sample.model.tables.pojos.Subject;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
@@ -23,14 +22,22 @@ public class SubjectRepository {
             Tables.SUBJECT.AMOUNT,
             Tables.SUBJECT.CREATE_DATE)
         .values(
-            subject.getName(),
-            subject.getComment(),
-            subject.getAmount(),
-            LocalDateTime.now())
+            subject.getName(), subject.getComment(), subject.getAmount(), subject.getCreateDate())
         .execute();
   }
 
   public List<Subject> getAll() {
     return dslContext.selectFrom(Tables.SUBJECT).fetchInto(Subject.class);
+  }
+
+  public int update(Subject subject) {
+    return dslContext
+        .update(Tables.SUBJECT)
+        .set(Tables.SUBJECT.NAME, subject.getName())
+        .set(Tables.SUBJECT.COMMENT, subject.getComment())
+        .set(Tables.SUBJECT.AMOUNT, subject.getAmount())
+        .set(Tables.SUBJECT.UPDATE_DATE, subject.getUpdateDate())
+        .where(Tables.SUBJECT.ID.eq(subject.getId()))
+        .execute();
   }
 }
