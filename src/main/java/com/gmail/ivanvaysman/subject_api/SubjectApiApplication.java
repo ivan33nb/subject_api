ca07aad124114fdcf64e896bddc102da20c9a738
out-stroki-waisman <k5LpLsM5Kgjy>
2022-09-03 21:37:38 +0300

    Initial commit

---
+++ src/main/java/com/gmail/ivanvaysman/subject_api/SubjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.ivanvaysman.subject_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SubjectApiApplication.class, args);
+  }
+
+}
