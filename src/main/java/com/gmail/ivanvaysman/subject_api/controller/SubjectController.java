26844bf3a9a687dda4d7ee468895b32d686ff17a
out-stroki-waisman <k5LpLsM5Kgjy>
2022-09-04 23:00:01 +0300

    Removed unusable endpoints from controller

--- src/main/java/com/gmail/ivanvaysman/subject_api/controller/SubjectController.java
+++ src/main/java/com/gmail/ivanvaysman/subject_api/controller/SubjectController.java
@@ -1,18 +1,12 @@
 package com.gmail.ivanvaysman.subject_api.controller;
 
-import com.gmail.ivanvaysman.subject_api.model.SubjectInsertReq;
-import com.gmail.ivanvaysman.subject_api.model.SubjectUpdateReq;
+import com.gmail.ivanvaysman.jooq.model.tables.pojos.Subject;
 import com.gmail.ivanvaysman.subject_api.service.SubjectService;
-import com.gmail.ivanvaysman.subject_api.service.impl.SubjectServiceImpl;
-import com.tej.JooQDemo.jooq.sample.model.tables.pojos.Subject;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,23 +17,8 @@ public class SubjectController {
 
   private final SubjectService service;
 
-  @GetMapping("/all")
-  public ResponseEntity<List<Subject>> getAll(){
-    return ResponseEntity.ok(service.getAll());
-  }
-
-  @PostMapping
-  public ResponseEntity<Integer> insert(@RequestBody SubjectInsertReq requestBody){
-    return ResponseEntity.ok(service.insert(requestBody));
-  }
-
-  @PutMapping
-  public ResponseEntity<Integer> update(@RequestBody SubjectUpdateReq requestBody){
-    return ResponseEntity.ok(service.update(requestBody));
-  }
-
   @GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<List<Subject>> getJson(){
+  public ResponseEntity<List<Subject>> getJson() {
     return ResponseEntity.ok(service.getAll());
   }
 }
